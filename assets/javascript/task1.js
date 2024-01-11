c093770ed6c46dce3470b9705b2f8cc65182487c
Rhys <rhys.moore@vtsd.com>
2024-01-11 11:46:07 -0500

    test day 1

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,14 @@
 /* Put code for the task below */
+const task1Button = document.getElementById("buttonTask1");
+const image = document.getElementById("imageTask1")
+
+// Show button when mouse hovers on image
+function showButton(){
+    task1Button.style.display = "block";
+    alert("It Works!");
+}
+
+// Hide image when click button
+function hideImage(){
+    image.style.display = "none";
+}
--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,4 @@
 /* Put code for the task below */
+const redButton = document.getElementById("task2Button1")
+const yellowButton = document.getElementById("task2Button2")
+const blueButton = document.getElementById("task2Button3")
