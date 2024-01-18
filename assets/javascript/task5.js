81cf37e6ad6f443d187b4b0745f68e067e172ce2
11826 <rhys.moore@vtsd.com>
2024-01-18 11:07:27 -0500

    Tasks 5 not complete

--- assets/javascript/task5.js
+++ assets/javascript/task5.js
@@ -1,1 +1,49 @@
 /* Put code for the task below */
+
+// Gain Access to form elements
+const submitButton = document.getElementById("submit");
+const nameField = document.getElementById("fullName");
+const addressField = document.getElementById("address");
+const emailField = document.getElementById("email");
+const phoneField = document.getElementById("phone");
+
+// Add event listener to submit button
+submitButton.addEventListener('click', checkForm);
+
+
+function checkForm(){
+    /*
+        In this method check whether name, email, and phone number have input.
+        If they are blank turn the fields red.  Otherwise do nothing.
+     */
+
+    // Check name field
+    if(nameField.value === ''){
+        nameField.style.backgroundColor = 'red';
+    }else{
+        nameField.style.backgroundColor = 'white';
+    }
+
+    // Check address field
+    if(addressField.value === ''){
+        nameField.style.backgroundColor = 'red';
+    }else{
+        nameField.style.backgroundColor = 'white';
+    }
+
+    // Check phone field
+    if(phoneField.value === ''){
+        phoneField.style.backgroundColor = 'red';
+    }else{
+        phoneField.style.backgroundColor = 'white';
+    }
+
+    // Check email field
+    if(emailField.value === ''){
+        emailField.style.backgroundColor = 'red';
+    }else{
+        emailField.style.backgroundColor = 'white';
+    }
+
+    alert("It is hooked up!");
+}
